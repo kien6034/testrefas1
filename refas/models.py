a145d9dfed2224538b3c2cdd855dc52626ecd4c2
kien6034 <trinhtrungkientb99@gmail.com>
2020-06-10 22:23:52 +0700

    Test model

--- refas/models.py
+++ refas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Archive(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.name}"
--- refas/views.py
+++ refas/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
-    return render(request, "refas/home.html")
+    archives = Archive.objects.all()
+    context = {
+        'archives': archives
+    }
+    return render(request, "refas/home.html", context = context)
